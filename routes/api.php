63736daa17d706d974d8473366acaa1f7b103bfa
Sohaila Farouk <sohailafarouk88@gmail.com>
2024-04-26 15:11:37 +0300

    routes and api added

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VoucherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +24,29 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register',[UserController::class, 'register']);
 Route::post('login',[UserController::class, 'login']);
+Route::post('logout',[UserController::class, 'logout']);
+
+
 Route::group(['prefix' => 'products'], function () {
-    Route::get('/', [ProductController::class, 'index']); // Get all products
-    Route::post('/store', [ProductController::class, 'store']); // Store a new product
-    Route::get('/show', [ProductController::class, 'show']); // Show details of a product
-    Route::put('/update', [ProductController::class, 'update']); // Update a product
-    Route::delete('/delete', [ProductController::class, 'destroy']); // Delete a product
+    Route::get('/', [ProductController::class, 'index']); 
+    Route::post('/store', [ProductController::class, 'store']);
+    Route::get('/show', [ProductController::class, 'show']);
+    Route::put('/update', [ProductController::class, 'update']); 
+    Route::delete('/delete', [ProductController::class, 'destroy']); 
 });
+Route::group(['prefix' => 'events'], function () {
+    Route::get('/', [EventController::class, 'index']); // Get all events
+    Route::post('/store', [EventController::class, 'store']); // Store a new event
+    Route::get('/show', [EventController::class, 'show']); // Show details of an event
+    Route::put('/update', [EventController::class, 'update']); // Update an event
+    Route::delete('/delete', [EventController::class, 'destroy']); // Delete an event
+});
+Route::group(['prefix' => 'vouchers'], function () {
+    Route::get('/', [VoucherController::class, 'index']); // Get all vouchers
+    Route::post('/store', [VoucherController::class, 'store']); // Store a new voucher
+    Route::get('/show', [VoucherController::class, 'show']); // Show details of an voucher
+    Route::put('/update', [VoucherController::class, 'update']); // Update an voucher
+    Route::delete('/delete', [VoucherController::class, 'destroy']); // Delete an voucher
+});
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VoucherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +21,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::post('register',[UserController::class, 'register']);
+Route::post('login',[UserController::class, 'login']);
+Route::post('logout',[UserController::class, 'logout']);
+
 Route::group(['prefix' => 'products'], function () {
     Route::get('/', [ProductController::class, 'index']); // Get all products
     Route::post('/store', [ProductController::class, 'store']); // Store a new product
     Route::get('/show', [ProductController::class, 'show']); // Show details of a product
     Route::put('/update', [ProductController::class, 'update']); // Update a product
     Route::delete('/delete', [ProductController::class, 'destroy']); // Delete a product
+});
+Route::group(['prefix' => 'events'], function () {
+    Route::get('/', [EventController::class, 'index']); // Get all events
+    Route::post('/store', [EventController::class, 'store']); // Store a new event
+    Route::get('/show', [EventController::class, 'show']); // Show details of an event
+    Route::put('/update', [EventController::class, 'update']); // Update an event
+    Route::delete('/delete', [EventController::class, 'destroy']); // Delete an event
+});
+Route::group(['prefix' => 'vouchers'], function () {
+    Route::get('/', [VoucherController::class, 'index']); // Get all vouchers
+    Route::post('/store', [VoucherController::class, 'store']); // Store a new voucher
+    Route::get('/show', [VoucherController::class, 'show']); // Show details of an voucher
+    Route::put('/update', [VoucherController::class, 'update']); // Update an voucher
+    Route::delete('/delete', [VoucherController::class, 'destroy']); // Delete an voucher
 });
